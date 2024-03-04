87ef8d46cd6bcd009bc1fb49dc9adecd87cfe840
jcpaeztello <141853470+jcpaeztello@users.noreply.github.com>
2024-03-03 23:01:57 -0500

    Nueva presentacion

--- Condiciones.ts
+++ Condiciones.ts
@@ -3,5 +3,5 @@ console.log("hola uniguajira");
 let nombre = "Jose paez"
 let nombre1= "Allenmar sierra"
 let nombre2= "Jairo arrefonfo"
-console.log("Hola ${nombre},${nombre1},${nombre2} Bienvenidos a Uniguajira");
+console.log("Hola ", {nombre},{nombre1},{nombre2}, " Bienvenidos a Uniguajira");
 
