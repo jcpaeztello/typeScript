b53ac2f0e42cd2fc455e22a70dced5fd870c0ba6
jcpaeztello <141853470+jcpaeztello@users.noreply.github.com>
2024-03-03 22:58:46 -0500

    Presentacion

--- Condiciones.ts
+++ Condiciones.ts
@@ -1,1 +1,7 @@
 console.log("hola uniguajira");
+
+let nombre = "Jose paez"
+let nombre1= "Allenmar sierra"
+let nombre2= "Jairo arrefonfo"
+console.log(`Hola ${nombre},${nombre1},${nombre2} Bienvenidos a Uniguajira`);
+
